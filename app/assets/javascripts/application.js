94fb3290c6ed7a516aa8d37761e5896dfbc434ac
Jean de Klerk <jadekler@gmail.com>
2015-07-10 20:52:26 -0600

    Added pipeline for semantic ui and jquery locally

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,8 @@
+// https://github.com/rails/sprockets#sprockets-directives
+
+//= require jquery-2.1.1.min
 //= require react
 //= require react_ujs
-//= require semantic.min
+//= require dist/semantic.min
+//= require_directory ../../../vendor/assets/semantic/dist/components/min/.
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 Bundler.require(*Rails.groups)
 
