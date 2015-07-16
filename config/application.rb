04e9b3df8b21872bfa5f08a03a682dfdf6268638
Jean de Klerk <jadekler@gmail.com>
2015-07-16 12:49:34 -0600

    Added rufus scheduler to run import task every 5 minutes. Should finish issue #9.

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,21 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rufus-scheduler'
 
 Bundler.require(*Rails.groups)
 
 module Pong
   class Application < Rails::Application
   end
+end
+
+scheduler = Rufus::Scheduler.new
+
+Rake::Task.clear
+Pong::Application.load_tasks
+
+scheduler.every '5m' do
+  Rake::Task[:import_racquet_data].reenable # in case you're going to invoke the same task second time.
+  Rake::Task[:import_racquet_data].invoke
 end
