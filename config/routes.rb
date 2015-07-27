7ec142854a6b38888428eba8b687771bac6ad813
Austin McKinley <austin.mckinley@acumendigital.com>
2015-07-27 13:03:10 -0600

    Refactored routes to use scope for all club-specific routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  get '/about', to: 'pong#about'
-
   get '/', to: 'pong#clubs'
+  get '/about', to: 'pong#about'
   get '/clubs', to: 'pong#clubs'
-
-  get '/:club/feed', to: 'pong#feed'
-  get '/:club/feed/data', to: 'pong#feeddata'
   get '/feed', to: 'pong#feeddata'
-
-  get '/:club/leaderboard', to: 'pong#leaderboard'
-  get '/:club/leaderboard/data', to: 'pong#leaderboarddata'
   get '/leaderboard', to: 'pong#leaderboard'
+
+  scope '/:club' do
+    get '/feed', to: 'pong#feed'
+    get '/feed/data', to: 'pong#feeddata'
+    get '/leaderboard', to: 'pong#leaderboard'
+    get '/leaderboard/data', to: 'pong#leaderboarddata'
+    post '/matches', to: 'pong#matches'
+  end
 end
