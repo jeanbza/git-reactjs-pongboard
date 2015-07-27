a7b01e1cf36f9b335752f1585691456cbe5cdec2
Jean de Klerk <jadekler@gmail.com>
2015-07-27 13:01:11 -0600

    work on getting a club add page per issue #6

---
+++ app/assets/javascripts/clubs.js
@@ -0,0 +1,11 @@
+// https://github.com/rails/sprockets#sprockets-directives
+
+//= require jquery-2.1.1.min
+//= require react
+//= require react_ujs
+//= require dist/semantic.min
+//= require_directory ../../../vendor/assets/semantic/dist/components/min/.
+
+$("document").ready(function() {
+  $('select.dropdown').dropdown();
+});
