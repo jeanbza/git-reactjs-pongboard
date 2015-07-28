4062b8d2fc2b8d254e14988aaad7091cd3191e50
Jean de Klerk <jadekler@gmail.com>
2015-07-27 19:21:47 -0600

    More work towards issue #6 - css working properly. Need to add more countries to form and a submission handler

--- app/assets/javascripts/clubs.js
+++ app/assets/javascripts/clubs.js
@@ -1,11 +1,7 @@
-// https://github.com/rails/sprockets#sprockets-directives
-
 //= require jquery-2.1.1.min
-//= require react
-//= require react_ujs
 //= require dist/semantic.min
-//= require_directory ../../../vendor/assets/semantic/dist/components/min/.
+//= require_relative ../../../vendor/assets/semantic/dist/components/min/dropdown.min
 
 $("document").ready(function() {
-  $('select.dropdown').dropdown();
+  $('.ui.dropdown').dropdown();
 });
