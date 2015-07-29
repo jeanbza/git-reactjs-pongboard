cb9120d06c42e09413c160f3415bb8b98c91a9ea
Brian Rose <brose@pivotallabs.com>
2015-07-29 01:28:00 -0600

    Use a hash default in calculating the leaderboard.

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,23 +1,14 @@
 class LeaderboardsController < ApplicationController
   def data
-    players = {}
+    players = Hash.new do |hash, key|
+      hash[key] = Elo::Player.new
+    end
 
     formatted_club_name = params[:club_id].gsub(/-/, ' ')
     matches = Match.joins(:club).where('lower(clubs.name) = ?', formatted_club_name).order(created_at: 'desc')
 
     matches.each do |match|
-        winner = match['winner']
-        loser = match['loser']
-
-      if (!players.has_key? winner)
-        players[winner] = Elo::Player.new
-      end
-
-      if (!players.has_key? loser)
-        players[loser] = Elo::Player.new
-      end
-
-      players[winner].wins_from(players[loser])
+      players[match.winner].wins_from(players[match.loser])
     end
 
     sorted_players = players.sort_by { |name, player| player.rating}.reverse!
